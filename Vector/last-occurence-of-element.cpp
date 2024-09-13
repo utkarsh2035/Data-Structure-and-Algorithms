90a7c7da3de73ce249250988ba683051fdbfbc49
Utkarsh Rawat <utkarshrawat2035@gmail.com>
2024-09-13 23:30:34 +0530

    Find last occurence of given element

--- Vector/last-occurence-of-element.cpp
+++ Vector/last-occurence-of-element.cpp
@@ -14,8 +14,11 @@ int main(){
     cout<<"enter element to find it's last occurence: "<<endl;
     cin>>target;
     int lastOccurence = -1;
-    for(int i = 0; i<size; i++){
-        if(nums[i] == target) lastOccurence = i;
+    for(int i = size-1; i>=0; i--){
+        if(nums[i] == target){
+            lastOccurence = i;
+            break;
+        }
     }
     cout<<"last occurence of element is at index: "<<lastOccurence<<endl;
 }
